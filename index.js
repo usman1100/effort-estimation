fa28682fc0eab003210c743ce0f6d0fe9796805a
usman1100 <usmanahmed1100@gmail.com>
2021-03-07 19:39:20 +0500

    sessions now working

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const bodyParser = require('body-parser');
+const session = require("express-session")
 const calc = require("./calculations")
+
+
 const app = express();
 const port = 1313;
 
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'ejs');
+// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded()); //Parse URL-encoded bodies
 app.use(express.static("public"));
-
+app.set('view engine', 'ejs');
+app.use(session({
+    secret: 'keyboard cat',
+  }))
 
 app.get("/", (req, res)=>{
     return res.render("index.ejs")
@@ -22,6 +28,12 @@ app.post("/model1post", (req, res)=>{
     console.log(req.body);
     let data = calc.UCP(req.body);
     return res.json(data)
+})
+
+app.get("/sus",(req, res)=>{
+    if(!req.session.view) req.session.view=1;
+    else req.session.view+=1;
+    res.json(req.session.view)
 })
 
 
